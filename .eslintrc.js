65a79202fbdf634db5ac8144da72d6132ea5cfea
pahval <42677400+pahval@users.noreply.github.com>
2020-08-22 05:40:20 +0200

    Add files via upload

---
+++ .eslintrc.js
@@ -0,0 +1,21 @@
+module.exports = {
+  root: true,
+  env: {
+    browser: true,
+    node: true,
+  },
+  parserOptions: {
+    parser: 'babel-eslint',
+  },
+  extends: [
+    '@nuxtjs',
+    'prettier',
+    'prettier/vue',
+    'plugin:prettier/recommended',
+    'plugin:nuxt/recommended',
+    'eslint:recommended',
+  ],
+  plugins: ['prettier', 'vue'],
+  // add your custom rules here
+  rules: {},
+}
